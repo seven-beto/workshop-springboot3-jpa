bdd1d7cbe30a40dbe204f3b80ac8eda93f97d6d7
unknown <herberthfreitas26@gmail.com.br>
2024-12-01 14:41:46 -0300

    Update User

--- course/src/main/java/com/educandoweb/course/controller/UserResourceController.java
+++ course/src/main/java/com/educandoweb/course/controller/UserResourceController.java
@@ -40,4 +40,10 @@ public class UserResourceController {
         return  ResponseEntity.noContent().build();
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update (@PathVariable Long id, @RequestBody User obj){
+        obj = userService.update(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
+
 }
