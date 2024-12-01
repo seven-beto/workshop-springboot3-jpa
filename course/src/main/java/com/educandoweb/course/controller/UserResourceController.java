dfe234f5eef67b2c50a493ea35340540373cc648
unknown <herberthfreitas26@gmail.com.br>
2024-12-01 13:53:01 -0300

    User Insert

--- course/src/main/java/com/educandoweb/course/controller/UserResourceController.java
+++ course/src/main/java/com/educandoweb/course/controller/UserResourceController.java
@@ -5,10 +5,7 @@ import com.educandoweb.course.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,6 +26,12 @@ public class UserResourceController {
     public ResponseEntity<User> findById(@PathVariable Long id){
         User obj = userService.findById(id);
         return new ResponseEntity<>(obj, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insertUser(@RequestBody User obj){
+        User user = userService.insert(obj);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
 }
