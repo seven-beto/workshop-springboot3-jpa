624c7b3268e82f18e92ca02e7da20e35f6381030
unknown <herberthfreitas26@gmail.com.br>
2024-12-04 21:29:28 -0300

    Exception update

--- course/src/main/java/com/educandoweb/course/service/UserService.java
+++ course/src/main/java/com/educandoweb/course/service/UserService.java
@@ -4,6 +4,7 @@ import com.educandoweb.course.entities.User;
 import com.educandoweb.course.exception.DatabaseException;
 import com.educandoweb.course.exception.ResourceNotFoundException;
 import com.educandoweb.course.repositories.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -42,9 +43,13 @@ public class UserService {
     }
 
     public User update (Long id, User obj){
-        User entity =  repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+       try{
+           User entity =  repository.getReferenceById(id);
+           updateData(entity, obj);
+           return repository.save(entity);
+       }catch (EntityNotFoundException e) {
+           throw new ResourceNotFoundException(id);
+       }
     }
 
     private void updateData(User entity, User obj) {
